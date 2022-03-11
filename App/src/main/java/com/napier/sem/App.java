4c970d5579cf1a05ee74258676e7240564df6147
jacques-andre <jacquesandre@mailbox.org>
2022-03-11 16:14:08 +0000

    Sorting population in App.java

--- App/src/main/java/com/napier/sem/App.java
+++ App/src/main/java/com/napier/sem/App.java
@@ -7,15 +7,24 @@ import java.util.*;
 
 public class App {
   public static void main(String[] args) {
-
+    largestPopulationToSmallest();
   }
 
   public static void largestPopulationToSmallest() {
     List<Country> all = Country.getCountries();
 
-    for (Country country : all) {
-      // loop through list, uses ToString
-      System.out.println(country);
+    // sort
+    Collections.sort(all, new Comparator<Country>() {
+      public int compare(Country o1, Country o2) {
+        return o1.Population - o2.Population;
+      }
+    });
+    for (int i = all.size() - 1; i >= 0; i--) {
+      // current iteration
+      Country currentCountry = all.get(i);
+      String out = String.format("Name:%s,Population:%d", currentCountry.Name, currentCountry.Population);
+
+      System.out.println(out);
     }
 
   }
