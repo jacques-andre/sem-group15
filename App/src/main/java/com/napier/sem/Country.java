dce010785a3d690d70e4acae83aa0197a5bc2086
Gregor Christie <gregor552@icloud.com>
2022-03-11 15:18:09 +0000

    added a semicolon lol

--- App/src/main/java/com/napier/sem/Country.java
+++ App/src/main/java/com/napier/sem/Country.java
@@ -2,7 +2,7 @@ public class Country {
     public int id;
     public String code;
     public String name;
-    public String continent //eventually make this link to continent class;
+    public String continent; //eventually make this link to continent class;
     public String Region;
     public int population;
     public String capital; //link to city class
