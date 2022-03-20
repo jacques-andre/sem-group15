212751ea46f338cbfb9ad88ef55f7d4c6cc3b86a
jacques-andre <jacquesandre@mailbox.org>
2022-03-20 23:25:43 +0000

    Testing new github actions with unit test

--- App/src/main/java/com/napier/sem/Database.java
+++ App/src/main/java/com/napier/sem/Database.java
@@ -6,13 +6,13 @@ import java.sql.DriverManager;
 
 public class Database {
   // constants
-  private static String DB_URL =
-      "jdbc:mysql://db:3306/world"; // using docker contianer name as host
+  private static String DB_URL = "jdbc:mysql://localhost:3306/world"; // using docker contianer name as host
   private static String DB_USERNAME = "root";
   private static String DB_PASSWORD = "password";
 
   /**
    * Attempt to connect to the docker mysql db
+   * 
    * @return connection to interact with db
    */
   public static Connection dbConnect() {
