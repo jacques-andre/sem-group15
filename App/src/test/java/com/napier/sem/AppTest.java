95fe6f714d5e2b3773feecbfc2d9db97606aabb5
jacques-andre <jacquesandre@mailbox.org>
2022-04-05 23:19:20 +0100

    Added AppTest: "testCountryRegionOrgnaisedLargestSmallest"

--- App/src/test/java/com/napier/sem/AppTest.java
+++ App/src/test/java/com/napier/sem/AppTest.java
@@ -12,7 +12,7 @@ public class AppTest {
 
   /**
    * Test if App.countryLargestPopulationToSmallest() returns correct number of
-   * countries,
+   * countries, and is in correct order (population descending)
    */
   @Test
   public void testCountryLargestPopulationToSmallest() throws Exception {
@@ -43,7 +43,7 @@ public class AppTest {
 
   /**
    * Test if App.countryContinentOrgnaisedLargestSmallest() returns correct
-   * number of countries,
+   * number of countries, and is in correct order (population descending)
    */
   @Test
   public void testCountryContinentOrgnaisedLargestSmallest() throws Exception {
@@ -53,6 +53,38 @@ public class AppTest {
     int expectedLen = 51;
 
     CountryReport result = App.countryContinentOrgnaisedLargestSmallest("Asia");
+    int gotLen = result.countryOutput.size();
+
+    assertEquals(expectedLen, gotLen);
+    System.out.printf("Expected:%d,Got:%d \n", expectedLen, gotLen);
+
+    // Loop through countryOutput, check if descending
+    for (int i = 0; i < result.countryOutput.size() - 1; i++) {
+      Country currentCountry = result.countryOutput.get(i);
+      Country nextCountry = result.countryOutput.get(i + 1);
+
+      // check if current iteration (i) population is not greater than i+1,
+      // if so throw err
+      if (!(currentCountry.Population >= nextCountry.Population)) {
+        throw new Exception("Population is not descending!");
+      }
+    }
+  }
+
+  /**
+   * Test if App.countryRegionOrgnaisedLargestSmallest() returns correct
+   * number of countries, and is in correct order (population descending)
+   */
+  @Test
+  public void testCountryRegionOrgnaisedLargestSmallest() throws Exception {
+    System.out.println("Testing:testCountryRegionOrgnaisedLargestSmallest");
+
+    String exampleRegion = "Caribbean";
+
+    // total rows in the country table with exampleRegion
+    int expectedLen = 24;
+
+    CountryReport result = App.countryRegionOrgnaisedLargestSmallest(exampleRegion);
     int gotLen = result.countryOutput.size();
 
     assertEquals(expectedLen, gotLen);
